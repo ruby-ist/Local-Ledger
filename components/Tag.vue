ca65929cca0016ee0b180afebf4c6a63494c9cc9
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2024-08-17 16:52:11 +0530

    Tags done

---
+++ components/Tag.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="tags">
+    <span class="name">{{ name }}</span>
+    <div class="icons">
+      <TagColor class="color" :color="color" />
+      <EditIcon />
+      <TrashIcon />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default defineNuxtComponent({
+  props: {
+    color: String,
+    name: String,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.tags {
+  padding: 20px 0 20px 36px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  .icons {
+    width: 50%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    align-items: center;
+
+    svg {
+      cursor: pointer;
+      height: 22px;
+    }
+  }
+
+  .name {
+    font-size: 1.3em;
+    font-weight: 500;
+  }
+
+  .color {
+    width: 20px;
+    height: 20px;
+    margin-right: 10px;
+  }
+}
+</style>
--- pages/tags.vue
+++ pages/tags.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-    <span>ToDo: Create Tags Component</span>
+    <Tag key="1" name="Food" color="5272f2" />
+    <Tag key="2" name="Rent" color="64CCC5" />
+    <Tag key="3" name="Snacks" color="9370db" />
+    <Tag key="4" name="Other" color="fee12b" />
+    <Tag key="5" name="Pending" color="f4a460" />
   </div>
 </template>
 
